9dfd5212c94075bc1139e465bd7b89484935e152
waithakakingalynette <warukiralynette@gmail.com>
2023-03-08 16:35:59 +0300

    first commit

---
+++ strings.js
@@ -0,0 +1,43 @@
+//Extract the last four characters from the string below;
+//"extravaganza"
+var y="extravaganza"
+console.log(y[8], y[9], y[10], y[11]);
+
+
+//Insert the following string at the fourth index of the below variable:
+//"eat"
+//const food = "The quick fox jumped over the lazy dog"
+const t="eat"
+const food=`The quick fox ${t} over the lazy dog`
+console.log(food)
+
+//Count how many times the following string appears in the string variable:
+//1. "the"
+//2."brown"
+const story= "The quick brown fox jumps over the lazy dog"
+let count=(story.ma
+    )
+const string2 = "The child was sitting on the table before it fell"
+let positions=(string2.search("sitting"));
+console.log(positions)
+
+
+//Convert the following strings into the specified format:
+//1. UpperCase: "wonderful"
+//2. LowerCase: "amazing", "UndERneath"
+const z = "wonderful"
+const w = z.toUpperCase()
+console.log(w)
+const d = "amazing"
+const e =d.toLowerCase()
+console.log(e);
+const f = "undERneath"
+const g = f.toLowerCase()
+console.log(g);
+const h = "A wonderful world"
+const i = h.toLocaleUpperCase()
+console.log(i);
+
+
+
+
